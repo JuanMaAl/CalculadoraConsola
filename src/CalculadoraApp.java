13a25e5879b4d20e3e864adb3170a93b9ce04646
Juan Martinez Alonso <juanMaAl1986@gmail.com>
2023-06-07 10:38:49 +0200

    Capturar excepciones generales provocadas por la entrada de datos incorrectos

--- src/CalculadoraApp.java
+++ src/CalculadoraApp.java
@@ -14,40 +14,45 @@ public class CalculadoraApp {
                     5.Salir
                     """);
             System.out.print("¿Operación a realizar? ");
-            var operacion = Integer.parseInt(consola.nextLine());
-            //Revisar que este dentro de las opciones mencionadas
-            if (operacion >= 1 && operacion <= 4) {
-                System.out.print("Proporciona valor operando1: ");
-                var operando1 = Integer.parseInt(consola.nextLine());
-                System.out.print("Proporciona valor operando2: ");
-                var operando2 = Integer.parseInt(consola.nextLine());
-                int resultado;
-                switch (operacion) {
-                    case 1 -> { //Suma
-                        resultado = operando1 + operando2;
-                        System.out.println("Resultado suman: " + resultado);
+            try {
+                var operacion = Integer.parseInt(consola.nextLine());
+                //Revisar que este dentro de las opciones mencionadas
+                if (operacion >= 1 && operacion <= 4) {
+                    System.out.print("Proporciona valor operando1: ");
+                    var operando1 = Integer.parseInt(consola.nextLine());
+                    System.out.print("Proporciona valor operando2: ");
+                    var operando2 = Integer.parseInt(consola.nextLine());
+                    int resultado;
+                    switch (operacion) {
+                        case 1 -> { //Suma
+                            resultado = operando1 + operando2;
+                            System.out.println("Resultado suman: " + resultado);
+                        }
+                        case 2 -> { //Resta
+                            resultado = operando1 - operando2;
+                            System.out.println("Resultado resta: " + resultado);
+                        }
+                        case 3 -> { //Multiplicación
+                            resultado = operando1 * operando2;
+                            System.out.println("Resultado multiplicación: " + resultado);
+                        }
+                        case 4 -> { //División
+                            resultado = operando1 / operando2;
+                            System.out.println("Resultado división: " + resultado);
+                        }
+                        default -> System.out.println("Opción errónea: " + operacion);
                     }
-                    case 2 -> { //Resta
-                        resultado = operando1 - operando2;
-                        System.out.println("Resultado resta: " + resultado);
-                    }
-                    case 3 -> { //Multiplicación
-                        resultado = operando1 * operando2;
-                        System.out.println("Resultado multiplicación: " + resultado);
-                    }
-                    case 4 -> { //División
-                        resultado = operando1 / operando2;
-                        System.out.println("Resultado división: " + resultado);
-                    }
-                    default -> System.out.println("Opción errónea: " + operacion);
+                } else if (operacion == 5) {
+                    System.out.println("Hasta pronto...");
+                    break;
+                } else {
+                    System.out.println("Opción errónea: " + operacion);
                 }
-            } else if (operacion == 5) {
-                System.out.println("Hasta pronto...");
-                break;
-            } else {
-                System.out.println("Opción errónea: " + operacion);
+                System.out.println(); //Imprime un salto de línea antes de repetir el ciclo
+            } //fin try
+            catch (Exception e) {
+                System.out.println("Ocurrió un error: " + e.getMessage() + "\n");
             }
-            System.out.println(); //Imprime un salto de línea antes de repetir el ciclo
         } //fin while
     } //fin main
 } //fin clase
