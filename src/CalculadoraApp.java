66a4145b01c354fa6e2b1384b9a3aa90a133ec19
Juan Martinez Alonso <juanMaAl1986@gmail.com>
2023-06-07 06:59:28 +0200

    Título del menú y suma en código duro (parámetros predeterminados)

--- src/CalculadoraApp.java
+++ src/CalculadoraApp.java
@@ -1,5 +1,11 @@
+import java.util.Arrays;
+
 public class CalculadoraApp {
     public static void main(String[] args) {
-        
+        System.out.println("**** Aplicación Calculadora ****");
+        var operando1 = 10;
+        var operando2 = 20;
+        var resultado = operando1 + operando2;
+        System.out.println("Resultado: " + resultado);
     }
 }
