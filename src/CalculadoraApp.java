ba60cc9c4ee3c0feede73c8d2652ec125d00cc9b
Juan Martinez Alonso <juanMaAl1986@gmail.com>
2023-06-07 07:08:23 +0200

    Suma recibiendo los valores por teclado

--- src/CalculadoraApp.java
+++ src/CalculadoraApp.java
@@ -1,10 +1,14 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class CalculadoraApp {
     public static void main(String[] args) {
         System.out.println("**** Aplicación Calculadora ****");
-        var operando1 = 10;
-        var operando2 = 20;
+        System.out.print("Proporciona valor operando1: ");
+        Scanner consola = new Scanner(System.in);
+        var operando1 = Integer.parseInt(consola.nextLine());
+        System.out.print("Proporciona valor operando2: ");
+        var operando2 = Integer.parseInt(consola.nextLine());
         var resultado = operando1 + operando2;
         System.out.println("Resultado: " + resultado);
     }
